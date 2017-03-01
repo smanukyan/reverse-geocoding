7229c970394c575a882cd564d0b5a2444af381a9
Sergey Manukyan <sergey.manukyan@gmail.com>
2017-03-01 01:06:26 -0500

    Added new @Cacheable method to lookup geocode and return object

--- src/main/java/com/manukyan/geocode/google/GoogleGeocodeService.java
+++ src/main/java/com/manukyan/geocode/google/GoogleGeocodeService.java
@@ -1,24 +1,37 @@
 package com.manukyan.geocode.google;
 
+import com.manukyan.geocode.Geocode;
 import com.manukyan.geocode.GeocodeService;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @Service
 public class GoogleGeocodeService implements GeocodeService {
 
     @Override
-    public String getAddressFromGeocode(double latitude, double longitude) {
-        Geocode geocode = getGeocode(latitude, longitude);
-        if (geocode.getResults().length == 0) {
-            return "No address returned";
-        }
-        return geocode.getResults()[0].getFormattedAddress();
+    @Cacheable("geocodes")
+    public Geocode lookupGeocode(double latitude, double longitude) {
+        return new Geocode(
+                latitude,
+                longitude,
+                lookupGeocodeAddress(latitude, longitude),
+                LocalDateTime.now());
     }
 
-    public Geocode getGeocode(double latitude, double longitude) {
+    public String lookupGeocodeAddress(double latitude, double longitude) {
+        GoogleGeocode googleGeocode = getGoogleGeocode(latitude, longitude);
+        if (googleGeocode.getResults().length == 0) {
+            return "No address returned";
+        }
+        return googleGeocode.getResults()[0].getFormattedAddress();
+    }
+
+    public GoogleGeocode getGoogleGeocode(double latitude, double longitude) {
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject(getUrl(latitude, longitude), Geocode.class);
+        return restTemplate.getForObject(getUrl(latitude, longitude), GoogleGeocode.class);
     }
 
     public String getUrl(double latitude, double longitude) {
