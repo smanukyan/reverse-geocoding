a21dfe0615f58f14148c1de187fe0f80d23c796c
Sergey Manukyan <sergey.manukyan@gmail.com>
2017-03-01 01:09:15 -0500

    Added tests for geocodeLookup

--- src/test/java/com/manukyan/geocode/google/GoogleGeocodeServiceTest.java
+++ src/test/java/com/manukyan/geocode/google/GoogleGeocodeServiceTest.java
@@ -1,37 +1,49 @@
 package com.manukyan.geocode.google;
 
+import com.manukyan.geocode.Geocode;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class GoogleGeocodeServiceTest {
 
+    private GoogleGeocodeService googleGeocodeService;
+
+    @Before
+    public void setUp() throws Exception {
+        this.googleGeocodeService = new GoogleGeocodeService();
+    }
+
     @Test
-    public void getGeocode() throws Exception {
-        GoogleGeocodeService geocodeService = new GoogleGeocodeService();
-        Geocode geocode = geocodeService.getGeocode(33.969601, -84.100033);
-        assertNotNull(geocode);
-        GeocodeResult[] results = geocode.getResults();
+    public void getGoogleGeocode() throws Exception {
+        GoogleGeocode googleGeocode = googleGeocodeService.getGoogleGeocode(33.969601, -84.100033);
+        assertNotNull(googleGeocode);
+        GoogleGeocodeResult[] results = googleGeocode.getResults();
         assertNotNull(results);
         assertTrue(results.length > 0);
     }
 
     @Test
     public void getUrl() throws Exception {
-        GoogleGeocodeService geocodeService = new GoogleGeocodeService();
         assertEquals(
                 "https://maps.googleapis.com/maps/api/geocode/json?latlng=33.969601,-84.100033",
-                geocodeService.getUrl(33.969601, -84.100033));
+                googleGeocodeService.getUrl(33.969601, -84.100033));
     }
 
     @Test
-    public void getAddressFromGeocode() throws Exception {
-        GoogleGeocodeService geocodeService = new GoogleGeocodeService();
-        assertEquals(
-                "2651 Satellite Blvd, Duluth, GA 30096, USA",
-                geocodeService.getAddressFromGeocode(33.969601, -84.100033));
+    public void lookupGeocodeAddress() throws Exception {
+        String geocodeAddress = googleGeocodeService.lookupGeocodeAddress(33.969601, -84.100033);
+        assertEquals("2651 Satellite Blvd, Duluth, GA 30096, USA", geocodeAddress);
     }
 
+    @Test
+    public void lookupGeocode() throws Exception {
+        Geocode geocode = googleGeocodeService.lookupGeocode(33.969601, -84.100033);
+        assertNotNull(geocode);
+        assertEquals("2651 Satellite Blvd, Duluth, GA 30096, USA", geocode.getAddress());
+        assertEquals(33.969601, geocode.getLatitude(), 0);
+        assertEquals(-84.100033, geocode.getLongitude(), 0);
+        assertNotNull(geocode.getDate());
+    }
 }
